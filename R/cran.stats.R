a91dcc10e59424c2975ccf2b962a1094f3020066
Arun Srinivasan <aragorn168b@gmail.com>
2014-08-21 13:29:39 +0200

    The long overdue commit, finally!

---
+++ R/cran.stats.R
@@ -0,0 +1,277 @@
+## exported functions
+read_logs <- function(start, end, path="./", dir="cran-mirror", verbose=TRUE) {
+    
+    if (class(start) != "Date") {
+        warning("Coercing 'start' to Date class")
+        start = as.Date(start)
+    }
+    if (class(end) != "Date") {
+        warning("Coercing 'end' to Date class")
+        end = as.Date(end)
+    }
+    urls = urls_(seq(start, end, by="days"))
+    odir = file.path(path, dir)
+    read_logs_(urls, odir, verbose)
+}
+
+stats_logs <- function(dt, type="monthly", packages=c("data.table"), dependency=TRUE, duration=30L) {
+
+    dependency = as.logical(dependency)    
+    if (is.na(dependency))
+        stop("'dependency' must be logical TRUE/FALSE")    
+    if (dependency) {
+        if (!suppressMessages(require(devtools)))
+            stop("'devtools' could not be loaded")
+        deps = dep_stats(dt, packages, type, duration)
+        deps = deps[, list(dep_N=sum(dep_N)), by=c("package", "key")]
+        setkey(deps, package, key)
+    }
+    this = pkg_stats(dt, packages, type)
+    
+    # get final counts
+    if (dependency) ans = this[deps] else ans = this
+    ans
+}
+
+plot_logs <- function(dt) {
+    ans = copy(dt)
+    if (!suppressMessages(require(ggplot2))) 
+        stop("'ggplot2' couldn't be loaded")
+    title = "Stats for the given duration"
+    setnames(ans, "key", "Period")
+
+    if ("dep_N" %chin% names(ans)) {
+        if (!suppressMessages(require(gridExtra))) 
+            stop("'gridExtra' couldn't be loaded")
+
+        ans[, pkg_N := tot_N - dep_N][, tot_N := NULL]
+        
+        pl1 = ggplot(data = ans, aes(x=Period, y=pkg_N, fill = package)) + 
+        geom_bar(stat = "identity", position="dodge") + 
+        theme_bw() + labs(title=paste(title, ": Direct downloads only")) + 
+        scale_fill_brewer(palette="Set1") + 
+        # scale_y_continuous(breaks = round(seq(0L, max(ans$pkg_N), length.out=5L))) + 
+        theme(axis.text.x = element_text(angle=45, hjust=1))
+
+        pl2 = ggplot(data = ans, aes(x=Period, y=dep_N, fill = package)) + 
+        geom_bar(stat = "identity", position="dodge") + 
+        theme_bw() + labs(title=paste(title, ": Through dependencies only")) + 
+        scale_fill_brewer(palette="Set1") + 
+        # scale_y_continuous(breaks = round(seq(0L, max(ans$dep_N), length.out=5L))) + 
+        theme(axis.text.x = element_text(angle=45, hjust=1))
+        
+        pl = list(pl1, pl2)
+        pl = do.call(marrangeGrob, c(pl, list(nrow=2L, ncol=1L,  top=quote(NULL))))
+    } else {
+        setnames(ans, "tot_N", "pkg_N")
+        pl = ggplot(data = ans, aes(x=Period, y=pkg_N, fill = package)) + 
+        geom_bar(stat = "identity", position="dodge") + 
+        theme_bw() + labs(title=paste(title, ": Direct + Dependencies")) + scale_fill_brewer(palette="Set1") + 
+        # scale_y_continuous(breaks = round(seq(0L, max(ans$pkg_N), length.out=5L))) + 
+        theme(axis.text.x = element_text(angle=45, hjust=1))
+    }
+    pl
+}
+
+## internal functions
+urls_ <- function(days) {
+    if (is.character(days)) days = as.Date(days)
+    paste('http://cran-logs.rstudio.com/', year(days), '/', days, '.csv.gz', sep="")
+}
+
+match_ <- function(urls, path, regex=NULL, pattern=NULL, reverse=FALSE) {
+    f1 = basename(urls)
+    f2 = list.files(path, pattern=pattern)
+    if (!is.null(regex)) {
+        f1 = gsub(regex, "", f1)
+        f2 = gsub(regex, "", f2)
+    }
+    if (reverse) f2 %chin% f1 else f1 %chin% f2
+}
+
+verbose_ <- function(pre, i, tot, file) {
+    str = paste("\r", pre, " ", sep="")
+    str = paste(str, i, " of ", tot, " - ", sep="")
+    str = paste(str, file, " (", sep="")
+    str = paste(str, round(i/tot * 100), "%)", sep="")
+    cat(str)
+    NULL
+}
+
+download_ <- function(src, dest, ...) {
+    suppressWarnings(
+        download.file(url=src, destfile=dest, ...)
+    )
+}
+
+download_logs <- function(urls, path, verbose=TRUE) {
+    dir.create(path, showWarnings = FALSE)
+    idx = !(match_(urls, path, regex="\\.gz$"))
+    src  = urls[idx]
+    dest = file.path(path, basename(src))
+    tot  = length(src)
+    ans  = vector("logical", tot)
+    for ( i in seq_along(src)) {
+        if (verbose) verbose_("Fetching logs", i, tot, basename(src[i]))
+        tryCatch({
+            download_(src[i], dest[i], quiet=TRUE)
+            ans[i] = TRUE
+        }, error = function(e) {
+            warning(paste("Couldn't open url", src[i], sep=" "))
+            system(paste("rm", dest[i]))
+        })
+    }
+    invisible(ans)
+}
+
+unzip_logs <- function(urls, path, rec=1L, verbose=TRUE) {
+    idx = match_(urls, path, pattern="\\.gz$")
+    dest = file.path(path, basename(urls)[idx])
+    tot  = length(dest)
+    for (i in seq_along(dest)) {
+        if (verbose) verbose_("Unzipping logs", i, tot, basename(dest[i]))
+        status <- system(paste('gunzip', dest[i]), ignore.stderr=TRUE)
+        if (status) {
+            # Unzipping failed. Try downloading one more time
+            system(paste("rm", dest[i]))
+            this_url = urls_(gsub("\\..*$", "", basename(dest[i])))
+            ans = download_logs(this_url, path, verbose=FALSE)
+            if (ans && rec==1L) unzip_logs(this_url, path, rec+1L)
+            else warning(paste("Recursion limit reached, 
+               couldn't unzip file", basename(dest[i]), sep=" "))
+        }
+    }
+    invisible(NULL)
+}
+
+fread_logs <- function(urls, path, verbose=TRUE) {
+    idx  = match_(urls, path, regex="\\.gz$", pattern="\\.csv$", reverse=TRUE)
+    dest = file.path(path, list.files(path, pattern="\\.csv$")[idx])
+    tot  = length(dest)
+    sel  = c("date", "time", "package", "country", "ip_id")
+    ans  = lapply(seq_along(dest), function(i){
+        if (verbose) verbose_("Fread(ing) logs   ", i, tot, basename(dest[i]))
+        fread(dest[i], select=sel)
+    })
+    ans = rbindlist(ans)
+    keycols = c("package", "date", "time")
+    setkeyv(ans, keycols)
+    setcolorder(ans, c(keycols, setdiff(names(ans), keycols)))
+}
+
+read_logs_ <- function(urls, odir, verbose) {
+    download_logs(urls, odir, verbose)
+    unzip_logs(urls, odir, 1L, verbose)
+    fread_logs(urls, odir, verbose)
+}
+
+yearly  = function(x) { gsub("^(.*)-(.*)-(.*)$", "\\1", x, perl=TRUE) }
+monthly = function(x) { gsub("^(.*)-(.*)-(.*)$", "\\1-\\2", x, perl=TRUE) }
+daily   = function(x) { x }
+weekly  = function(x) {
+    week = yday(as.IDate(x)-1L)%/%7L + 1L
+    paste(yearly(x), week, sep='-')
+}
+hourly  = function(x, y) {
+    hour = gsub("^(.*):(.*):(.*)$", "\\1", y, perl=TRUE)
+    paste(daily(x), hour, sep=":")
+}
+
+key_ <- function(date, time, type) {
+    switch(type, yearly = yearly(date), 
+                monthly = monthly(date), 
+                 weekly = weekly(date),
+                  daily = daily(date),  
+                 hourly = hourly(date, time))
+}
+
+deps_ <- function(packages) {
+    dep_pkg = lapply(packages, revdep)
+    dep_len = vapply(dep_pkg, length, 0L)
+    package = rep(packages, dep_len)
+    uses_it = unlist(dep_pkg, use.names=FALSE)
+    dep_pkg = data.table(package = package, dependency = uses_it)
+    setkey(dep_pkg, dependency)
+}
+
+dep_stats <- function(dt, packages, type, duration) {
+
+    if (is.null(key(dt)) || key(dt)[1L] != "package")
+        stop("dt's first key column must be 'package'")
+    if (!is.character(packages)) stop("'packages' must be a character vector")
+    types = c("yearly", "monthly", "weekly", "daily", "hourly")
+    if (!is.character(type) || length(type) != 1L) 
+        stop("'type' must be a character vector of length 1")
+    if (!type %chin% types) 
+        stop(paste("'type' must be one of:", paste(types, collapse=","), sep=" "))
+    duration = suppressWarnings(as.integer(duration))
+    if (duration < 0L || is.na(duration)) 
+        stop("'duration' is in seconds, can't be a negative value, expecting >= 0L")
+
+    this = dt[packages][!is.na(date) & !is.na(time)][, key := key_(date, time, type)]
+    this[, date_time := as.integer(as.POSIXct(paste(date, time)))]
+    setkey(this, package, key)
+    
+    dep = dt[deps_(packages)][!is.na(date) & !is.na(time)]
+    dep[, key := key_(date, time, type)]
+    dep[, date_time := as.integer(as.POSIXct(paste(date, time)))]
+    setkey(dep, date_time)
+    
+    range_counts <- function(x, y) {
+        range1 = x$date_time - duration
+        range2 = x$date_time + duration
+        
+        index1 = y[J(range1), roll=-Inf, nomatch=NA, mult="first", which=TRUE]
+        index2 = y[J(range2), roll=+Inf, nomatch=NA, mult="last",  which=TRUE]
+        
+        start = pmin(index1,index2,na.rm=TRUE)
+        end   = pmax(index1,index2,na.rm=TRUE)
+        
+        diff_xy <- function(date_time, i) {
+            ii = start[i]:end[i]
+            aa = date_time
+            bb = y$date_time[ii]
+            ii[abs(bb-aa) <= duration]
+        }
+        ans = x[, list(yi=diff_xy(date_time, .I)), by=list(xi=seq_len(nrow(x)))]
+        ans[, `:=`(x.ip = x$ip_id[xi], y.ip = y$ip_id[yi])]
+        ans[, `:=`(x.cntry = x$country[xi], y.cntry = y$country[yi])]
+        idx = unique(ans[x.ip == y.ip & x.cntry == y.cntry]$yi)
+        y$package[idx]
+    }
+    
+    ans <- dep[, list(dep_pkg=range_counts(this[as.data.table(.BY)], .SD)), by=list(i.package, key)]
+    setnames(ans, "i.package", "package")
+    setkey(ans, package, key)
+    ans[, list(dep_N=.N), by=names(ans)]
+}
+
+pkg_stats <- function(dt, packages, type) {
+
+    if (is.null(key(dt)) || key(dt)[1L] != "package")
+        stop("dt's first key column must be 'package'")
+    if (!is.character(packages)) stop("'packages' must be a character vector")
+    types = c("yearly", "monthly", "weekly", "daily", "hourly")
+    if (!is.character(type) || length(type) != 1L) 
+        stop("'type' must be a character vector of length 1")
+    if (!type %chin% types) 
+        stop(paste("'type' must be one of:", paste(types, collapse=","), sep=" "))
+
+    this = dt[packages][!is.na(date) & !is.na(time)][, key := key_(date, time, type)]
+    ans = this[, list(tot_N = .N), by=list(package, key)]
+    setkey(ans, package, key)
+}
+
+
+# monthly_stats <- function(dat, packages=c("data.table")) {
+#     plot_stats(dat, "month", packages)
+# }
+# 
+# daily_stats <- function(dat, packages=c("data.table")) {
+#     plot_stats(dat, "day", packages)
+# }
+# 
+# yearly_stats <- function(dat, packages=c("data.table")) {
+#     plot_stats(dat, "year", packages)    
+# }
+# 
--- README.md
+++ README.md
@@ -1,4 +1,116 @@
-cran-log-vis
-============
+# cran.stats
 
-Visualise package download stats from Rstudio mirror by removing dependencies
+`cran.stats` is a package that provides functions to explore and visualise daily, monthly or yearly package download stats from Rstudio cran mirror logs. It also implements an approach to calculate the number of downloads for a given package alone by identifying and subtracting the downloads due to it's dependent packages.
+
+# Installation
+
+To install the package, use `devtools`:
+
+```R
+devtools::install_github("arunsrinivasan/cran.stats")
+```
+
+# Usage
+
+There are three functions to be aware of:
+
+### read_logs
+
+Here's the function definition:
+
+```R
+read_logs(start, end, path="./", dir="cran-mirror", verbose=TRUE)
+```
+
+The function takes a `start` and `end` date and downloads all the logs available from the Rstudio mirror for that duration and places them in the directory `path/dir`. The downloaded files are automatically replaced with their unzipped versions. If an unzipped log file already exists, then the downloads are skipped. And if the downloaded file was corrupted (which would error while unzipping), then we attempt to download it just once again. If it fails again, we skip this log.
+
+As an example, let's download all the logs between July 01, 2014 and July 31, 2014.
+
+```R
+dt = read_logs(start = as.Date("2014-07-01"), 
+               end = as.Date("2014-07-31"), 
+               path = "./", dir="cran-mirror", 
+               verbose = TRUE)
+
+dim(dt)
+# [1] 4824243       5
+
+key(dt)
+# [1] "package" "date"    "time"   
+
+dt["data.table"]
+#          package       date     time country ip_id
+#     1: data.table 2014-07-01 00:03:47      US    13
+#     2: data.table 2014-07-01 00:04:30      US    13
+#     3: data.table 2014-07-01 00:07:12      US     2
+#     4: data.table 2014-07-01 00:09:10      US    51
+#     5: data.table 2014-07-01 00:12:33      US    13
+#    ---                                             
+# 14393: data.table 2014-07-31 23:33:04      US  7159
+# 14394: data.table 2014-07-31 23:33:09      US  7100
+# 14395: data.table 2014-07-31 23:35:16      NZ  1185
+# 14396: data.table 2014-07-31 23:52:41      US 11181
+# 14397: data.table 2014-07-31 23:57:21      US  7159
+```
+
+### stats_logs
+
+Here's the function definition:
+
+```R
+stats_logs(dt, type="monthly", pacakges=c("data.table"), 
+          dependency=TRUE, duration = 30L)
+```
+
+Have a look at `?stats_logs` for more info. Let's keep it the defaults as such and get the download stats for the month of July. Note that we've to set the `type` to `daily` as we'd like to get the number of downloads on each day.
+
+```R
+dt_s = stats_logs(dt, type="daily")
+#        package        key tot_N dep_N
+#  1: data.table 2014-07-01   456   136
+#  2: data.table 2014-07-02   406    96
+#  3: data.table 2014-07-03   393    78
+#  4: data.table 2014-07-04   267    74
+#  5: data.table 2014-07-05   178    60
+#  6: data.table 2014-07-06   169    40
+#  7: data.table 2014-07-07   450   102
+#  8: data.table 2014-07-08   680   183
+#  9: data.table 2014-07-09   663   147
+# 10: data.table 2014-07-10   705   131
+# 11: data.table 2014-07-11   709   176
+# 12: data.table 2014-07-12   556    65
+# 13: data.table 2014-07-13   676    35
+# 14: data.table 2014-07-14   605   100
+# 15: data.table 2014-07-15   637    98
+# 16: data.table 2014-07-16   549   111
+# 17: data.table 2014-07-17   586    91
+# 18: data.table 2014-07-18   481   101
+# 19: data.table 2014-07-19   230    59
+# 20: data.table 2014-07-20   273    55
+# 21: data.table 2014-07-21   525   137
+# 22: data.table 2014-07-22   504    89
+# 23: data.table 2014-07-23   605   142
+# 24: data.table 2014-07-24   530   141
+# 25: data.table 2014-07-25   394   109
+# 26: data.table 2014-07-26   214    38
+# 27: data.table 2014-07-27   215    46
+# 28: data.table 2014-07-28   440   148
+# 29: data.table 2014-07-29   456   106
+# 30: data.table 2014-07-30   440   115
+# 31: data.table 2014-07-31   405    86
+#        package        key tot_N dep_N
+```
+
+### plot_logs
+
+Here's the function definition:
+
+```R
+plot_logs(dt)
+```
+
+Let's now plot the stats. Note that it detects the presenceo f `dep_N` column and automatically draws two plots, first with just the *direct* downloads for the packages specified, and the next with the number of downloads just due to dependencies.
+
+```R
+plot_logs(dt_s)
+```
